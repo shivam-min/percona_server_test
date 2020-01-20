751f004b174375b7753fc1a7b6711dc0f0f9f22c
shivam-min <shivam8139@gmail.com>
2020-01-20 21:26:12 +0530

    Update and rename install.sh to install_percona_mysql.sh

---
+++ install_percona_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#set the mysql root password
+MYSQL_ROOT_PASSWORD=root
+
+#Setup the officail percona apt-repository
+wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+sudo apt-get update
+
+echo "percona-server-server-5.7 percona-server-server-5.7/root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
+echo "percona-server-server-5.7 percona-server-server-5.7/re-root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
+
+#install the percona server package and set it up
+sudo apt-get install -y  percona-server-server-5.7
